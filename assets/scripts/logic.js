e6375574f9d676fccc0f0edd0e43157b79034e9b
Narasimhan Ramesh <narasimhan.ramesh5@gmail.com>
2018-09-30 00:18:01 -0700

    Console logs removed

--- assets/scripts/logic.js
+++ assets/scripts/logic.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
 	// into the system
 	$("#train-submit").on("click", function(){
 		event.preventDefault();
-		console.log("New train submitted");
+		//console.log("New train submitted");
 
 		var name = $("#name-input").val();
 		var destination = $("#destination-input").val();
@@ -34,7 +34,7 @@ $(document).ready(function(){
 
 		// ToDo: input validation
 		if(!name || !destination || !firstTime || !frequency){
-			console.log("Please fill in all fields");
+			//console.log("Please fill in all fields");
 			// TODO : Display a warning box 
 			return;
 		}
@@ -42,7 +42,7 @@ $(document).ready(function(){
 		// Train frequency must be a numeric value
 		// Use JQuery's 'isNumeric' for this
 		if(!($.isNumeric(frequency))){
-			console.log("Please specify train frequency in minutes - enter a number");
+			//console.log("Please specify train frequency in minutes - enter a number");
 			return;
 		}
 
@@ -51,9 +51,9 @@ $(document).ready(function(){
 
 		// First train time must be in the format HH:mm
 		var firstTimeMoment = moment("2018-01-01T"+firstTime);
-		console.log(firstTimeMoment);
+		//console.log(firstTimeMoment);
 		if(!firstTimeMoment.isValid()){
-			console.log("First time is not a valid moment in time");
+			//console.log("First time is not a valid moment in time");
 			return;
 		}
 
@@ -90,15 +90,15 @@ $(document).ready(function(){
 			// is stored as a user-defined attribute in the table.
 			// (The text is just the HH:mm formatted time)
 			if($(this).hasClass("next-arrival-cell")){
-				console.log($(this).text());
+				//console.log($(this).text());
 
 				nextArrivalMilliseconds = parseInt($(this).attr("next-arrival-val"));
 				if(nextArrivalMilliseconds){
-					console.log("Next arrival in milliseconds "+nextArrivalMilliseconds);
+					//console.log("Next arrival in milliseconds "+nextArrivalMilliseconds);
 				}
 
 				nextArrival = moment(nextArrivalMilliseconds);
-				console.log(nextArrival.format());
+				//console.log(nextArrival.format());
 
 				// Compute new minutes remaining based on current time
 				// If minutes remaining is zero, update to next occurrence
@@ -106,7 +106,7 @@ $(document).ready(function(){
 				if(minutesRemaining <= 0){
 					minutesRemaining += frequency;
 					nextArrival.add(minutesRemaining, "minutes");
-					console.log("Next Arrival at "+nextArrival.format());
+					//console.log("Next Arrival at "+nextArrival.format());
 
 					nextArrivalFormatted = nextArrival.format("HH:mm");
 					$(this).text(nextArrivalFormatted);
@@ -115,13 +115,13 @@ $(document).ready(function(){
 
 			if($(this).hasClass("minutes-remaining-cell")){
 				$(this).text(minutesRemaining);
-				console.log($(this).text());
+				//console.log($(this).text());
 			}
 		});
 
-		console.log(timerHandle);
+		//console.log(timerHandle);
 
-		console.log("END INTERVAL");
+		//console.log("END INTERVAL");
 	}
 
 	/**
@@ -145,7 +145,7 @@ $(document).ready(function(){
 
 		// Log everything that's coming out of snapshot
 		trainObject = snapshot.val();
-		console.log(trainObject);
+		//console.log(trainObject);
 
 		/* The train object must have the following attributes
 			 - trainName
@@ -155,14 +155,14 @@ $(document).ready(function(){
 
 		firstTime = trainObject.firstTime;
 
-		console.log("The first ever time of this train was ");
-		console.log(firstTime);
+		//console.log("The first ever time of this train was ");
+		//console.log(firstTime);
 
 		// Get the current time in EPOCH 
 		var now = moment();
 
-		console.log("The current time is ");
-		console.log(now);
+		//console.log("The current time is ");
+		//console.log(now);
 
 		// Get the difference between current time and first ever time in minutes
 		diffInMinutes = now.diff(moment(firstTime),"minutes");
@@ -171,11 +171,11 @@ $(document).ready(function(){
 		// as the next arrival time in HH:mm
 		remainder  = diffInMinutes % trainObject.trainFrequency;
 		minutesRemaining = trainObject.trainFrequency - remainder;
-		console.log("Next train arrives in "+minutesRemaining+" minutes");
+		//console.log("Next train arrives in "+minutesRemaining+" minutes");
 
 		nextArrival = now.add(minutesRemaining,"minutes");
 		nextArrivalFormatted = nextArrival.format("HH:mm");
-		console.log("Next arrival at "+nextArrivalFormatted);
+		//console.log("Next arrival at "+nextArrivalFormatted);
 
 		// Update the train schedule table
 		var newRow = $("<tr>");
@@ -214,12 +214,12 @@ $(document).ready(function(){
 		// Firebase callback gets invoked 
 		if(!timerRunning){
 			timerHandle = setInterval(refreshTimeRemaining, 1000);
-			console.log(timerHandle);
+			//console.log(timerHandle);
 			timerRunning = true;
 		}
 
 	}, function(errorObject) {// Error Handler
-		console.log("Error reaching database -  " + errorObject.code);
+		//console.log("Error reaching database -  " + errorObject.code);
 	});
 
 });
